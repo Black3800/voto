d258a458084dfb84818413be84f6e32de032aac0
anakint <anakin.t@mail.kmutt.ac.th>
2022-04-30 20:32:57 +0700

    Add status colors

--- voto_mobile/lib/utils/color.dart
+++ voto_mobile/lib/utils/color.dart
@@ -105,6 +105,46 @@ class VotoColors {
     900: Color.fromRGBO(242, 244, 248, 1),
   });
 
+  static const MaterialColor success = MaterialColor(0xff76ec36, {
+    50: Color.fromRGBO(118, 236, 54, .1),
+    100: Color.fromRGBO(118, 236, 54, .2),
+    200: Color.fromRGBO(118, 236, 54, .3),
+    300: Color.fromRGBO(118, 236, 54, .4),
+    400: Color.fromRGBO(118, 236, 54, .5),
+    500: Color.fromRGBO(118, 236, 54, .6),
+    600: Color.fromRGBO(118, 236, 54, .7),
+    700: Color.fromRGBO(118, 236, 54, .8),
+    800: Color.fromRGBO(118, 236, 54, .9),
+    900: Color.fromRGBO(118, 236, 54, 1),
+  });
+
+  static const MaterialColor warning = MaterialColor(0xffecc636, {
+    50: Color.fromRGBO(236, 198, 54, .1),
+    100: Color.fromRGBO(236, 198, 54, .2),
+    200: Color.fromRGBO(236, 198, 54, .3),
+    300: Color.fromRGBO(236, 198, 54, .4),
+    400: Color.fromRGBO(236, 198, 54, .5),
+    500: Color.fromRGBO(236, 198, 54, .6),
+    600: Color.fromRGBO(236, 198, 54, .7),
+    700: Color.fromRGBO(236, 198, 54, .8),
+    800: Color.fromRGBO(236, 198, 54, .9),
+    900: Color.fromRGBO(236, 198, 54, 1),
+  });
+
+  static const MaterialColor danger = MaterialColor(0xffec3636, {
+    50: Color.fromRGBO(236, 54, 54, .1),
+    100: Color.fromRGBO(236, 54, 54, .2),
+    200: Color.fromRGBO(236, 54, 54, .3),
+    300: Color.fromRGBO(236, 54, 54, .4),
+    400: Color.fromRGBO(236, 54, 54, .5),
+    500: Color.fromRGBO(236, 54, 54, .6),
+    600: Color.fromRGBO(236, 54, 54, .7),
+    700: Color.fromRGBO(236, 54, 54, .8),
+    800: Color.fromRGBO(236, 54, 54, .9),
+    900: Color.fromRGBO(236, 54, 54, 1),
+  });
+
+
   static const MaterialColor indigo = primary;
   static const MaterialColor magenta = secondary;
   static const MaterialColor flamingo = highlight;
