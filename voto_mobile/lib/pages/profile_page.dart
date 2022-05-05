2e11e17e0431d7d40a2d0d5e70aac283521497b1
anakint <anakin.t@mail.kmutt.ac.th>
2022-05-05 23:15:16 +0700

    Make ImageInput an input

--- voto_mobile/lib/pages/profile_page.dart
+++ voto_mobile/lib/pages/profile_page.dart
@@ -1,18 +1,55 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:voto_mobile/model/users.dart';
+import 'package:voto_mobile/widgets/image_input.dart';
 import 'package:voto_mobile/widgets/profile/profile_button.dart';
 import 'package:voto_mobile/widgets/profile/profile_display_name.dart';
 import 'package:voto_mobile/widgets/profile/profile_name_edit.dart';
 import 'package:voto_mobile/widgets/profile/profile_picture_edit.dart';
 import 'package:voto_mobile/widgets/voto_scaffold.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  String? displayName;
+  String? email;
+  String? image;
+
+  Future<void> _getProfile() async {
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      if(user != null) {
+        String uid = user.uid;
+        DatabaseReference ref = FirebaseDatabase.instance.ref('users/$uid');
+        ref.onValue.listen((event) {
+          if(event.snapshot.exists) {
+            final json = event.snapshot.value as Map<dynamic, dynamic>;
+            Users data = Users.fromJson(json);
+            setState(() {
+              displayName = data.displayName;
+              email = user.email;
+              image = data.img;
+            });
+          }
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getProfile();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final String displayName = FirebaseAuth.instance.currentUser?.displayName ?? 'Anonymous';
-    final String email = FirebaseAuth.instance.currentUser?.email ?? 'abc@example.com';
+    
     return VotoScaffold(
       title: 'Edit profile',
       useMenu: false,
@@ -22,9 +59,18 @@ class ProfilePage extends StatelessWidget {
         children: [
           Column(
             children: [
-              const ProfilePictureEditing(),
-              ProfileDisplayName(name: displayName),
-              ProfileDisplayNameEditing(name: displayName, email: email),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                child: ImageInput(
+                  image: image ?? 'gs://cs21-voto.appspot.com/dummy/blank.webp',
+                  radius: 150.0,
+                  onChanged: (newPath) {
+                    // setState(() => createTeamImage = newPath);
+                  },
+                ),
+              ),
+              ProfileDisplayName(name: displayName ?? ''),
+              ProfileDisplayNameEditing(name: displayName ?? '', email: email ?? ''),
             ],
           ),
           const ProfileButton(),
--- voto_mobile/lib/widgets/image_input.dart
+++ voto_mobile/lib/widgets/image_input.dart
@@ -1,51 +1,134 @@
+import 'dart:typed_data';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:uuid/uuid.dart';
 import 'package:voto_mobile/utils/color.dart';
 
 class ImageInput extends StatefulWidget {
+  final String image;
   final double radius;
-  final String? image;
   final String? initial;
-  const ImageInput({Key? key, this.image, this.initial, this.radius = 120.0}) : super(key: key);
+  final Function(String)? onChanged;
+  const ImageInput(
+      {Key? key,
+      required this.image,
+      this.onChanged,
+      this.initial,
+      this.radius = 120.0})
+      : super(key: key);
 
   @override
   State<ImageInput> createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
+  String? imageURL;
+  bool isLoading = true;
+
+  Future<void> _getImageURL() async {
+    imageURL = await FirebaseStorage.instance
+        .refFromURL(widget.image)
+        .getDownloadURL();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> _handleInput() async {
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(type: FileType.image);
+    if (result != null) {
+      if (mounted) setState(() => isLoading = true);
+      Uint8List fileBytes = result.files.first.bytes ?? Uint8List(0);
+      if (defaultTargetPlatform == TargetPlatform.iOS ||
+          defaultTargetPlatform == TargetPlatform.android) {
+        fileBytes = await FlutterImageCompress.compressWithList(
+          fileBytes,
+          minHeight: 1920,
+          minWidth: 1080,
+        );
+      }
+      const uuid = Uuid();
+      while (true) {
+        String fileName = uuid.v1();
+        String path = 'gs://cs21-voto.appspot.com/uploads/$fileName';
+        try {
+          await FirebaseStorage.instance.ref(path).getDownloadURL();
+          debugPrint('duplicate filename exists, getting a new name...');
+        } on FirebaseException catch (e) {
+          if (e.code == 'object-not-found') {
+            await FirebaseStorage.instance.ref(path).putData(fileBytes);
+            widget.onChanged?.call(path);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  Future<void> _deletePreviousImage(String? previousImage) async {
+    /***
+     * Delete previousImage if not in /dummy folder
+     */
+    if (previousImage != null && previousImage.split('/')[3] != 'dummy') {
+      FirebaseStorage.instance.ref(previousImage).delete();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getImageURL();
+  }
+
+  @override
+  void didUpdateWidget(covariant ImageInput oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _deletePreviousImage(oldWidget.image);
+    _getImageURL();
+  }
+
   @override
   Widget build(BuildContext context) {
-    double position = widget.radius * sin(pi/4);
+    double position =
+        widget.radius * 0.70710678118; // equals radius * sin(pi/4)
     double size = position + widget.radius / 3;
 
     return SizedBox(
       width: size,
       height: size,
       child: Stack(children: [
-        widget.image != null ? CircleAvatar(
-          backgroundImage: AssetImage(widget.image ?? ''),
-          radius: widget.radius
-        ) : CircleAvatar(
-          backgroundColor: VotoColors.indigo[300],
-          child: Text(widget.initial ?? '', style: Theme.of(context).textTheme.headline1),
-          radius: widget.radius
-        ),
+        CircleAvatar(
+            backgroundImage: isLoading ? null : NetworkImage(imageURL ?? ''),
+            backgroundColor: VotoColors.gray,
+            child: isLoading
+                ? Text('Loading',
+                    style: GoogleFonts.inter(color: VotoColors.black.shade300))
+                : null,
+            radius: widget.radius),
         Positioned(
             top: position,
             left: position,
             child: Container(
-              height: widget.radius / 3,
-              width: widget.radius / 3,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: VotoColors.primary,
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.camera_alt),
-                iconSize: widget.radius / 6,
-                onPressed: () {},
-                color: VotoColors.white,)
-            )),
+                height: widget.radius / 3,
+                width: widget.radius / 3,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: VotoColors.primary,
+                ),
+                child: Center(
+                  child: IconButton(
+                    icon: const Icon(Icons.camera_alt),
+                    iconSize: widget.radius / 6,
+                    onPressed: _handleInput,
+                    color: VotoColors.white,
+                  ),
+                ))),
       ]),
     );
   }
