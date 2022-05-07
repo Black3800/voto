f6e71c5ee11056bfd9bef25d57bbd6d7e6c4371b
anakint <anakin.t@mail.kmutt.ac.th>
2022-05-08 03:49:57 +0700

    Make Pass editable

--- voto_mobile/lib/widgets/manageteam/passcode.dart
+++ voto_mobile/lib/widgets/manageteam/passcode.dart
@@ -1,38 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:voto_mobile/widgets/manageteam/passcodefiled.dart';
 
-class Pass extends StatelessWidget {
+class Pass extends StatefulWidget {
   bool isEditing;
+  Function(String)? onChanged;
+  String? value;
+  Pass({
+    required this.isEditing,
+    this.onChanged,
+    this.value = '    ',
+    Key? key
+  }) : super(key: key);
 
-  Pass({required this.isEditing, Key? key}) : super(key: key);
+  @override
+  State<Pass> createState() => _PassState();
+}
+
+class _PassState extends State<Pass> {
+  final TextEditingController _newPasscodeController = TextEditingController();
+
+  Future<void> _changePasscode() async {
+    String? newPasscode = await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('New passcode'),
+        content: IntrinsicHeight(
+          child: Column(
+            children: [
+              const Text('Leave blank to remove passcode'),
+              TextFormField(
+                controller: _newPasscodeController,
+                maxLength: 4,
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                decoration: const InputDecoration(
+                  hintText: '4 digit number'
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, null),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, _newPasscodeController.text),
+            child: const Text('Save'),
+          ),
+        ],
+      )
+    );
+    if(newPasscode != null && newPasscode != widget.value) {
+      WidgetsBinding.instance?.addPostFrameCallback((_) => setState(() {
+        if(newPasscode.isEmpty) {
+          widget.onChanged?.call('');
+        } else if(newPasscode.length == 4) {
+          widget.onChanged?.call(newPasscode);
+        }
+      }));
+    }
+    
+  }
 
   @override
   Widget build(BuildContext context) {
-    int passNumber1 = 1;
-    int passNumber2 = 2;
-    int passNumber3 = 3;
-    int passNumber4 = 4;
-
-    return Container(
-      padding: const EdgeInsets.only(left: 28),
-      child: Row(children: [
-        Passcode(
-          passNumber: passNumber1,
-          isEditing: isEditing,
+    return InkWell(
+      onTap: _changePasscode,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(
+            4,
+            (index) => Passcode(passNumber: widget.value?[index] ?? '')
         ),
-        Passcode(
-          passNumber: passNumber2,
-          isEditing: isEditing,
-        ),
-        Passcode(
-          passNumber: passNumber3,
-          isEditing: isEditing,
-        ),
-        Passcode(
-          passNumber: passNumber4,
-          isEditing: isEditing,
-        ),
-      ]),
+      ),
     );
   }
 }
--- voto_mobile/lib/widgets/manageteam/passcodefiled.dart
+++ voto_mobile/lib/widgets/manageteam/passcodefiled.dart
@@ -3,9 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:voto_mobile/utils/color.dart';
 
 class Passcode extends StatefulWidget {
-  int passNumber;
+  String passNumber;
   bool isEditing;
-  Passcode({Key? key, required this.passNumber, required this.isEditing})
+  Passcode({Key? key, required this.passNumber, this.isEditing = false})
       : super(key: key);
 
   @override
@@ -16,29 +16,21 @@ class _PasscodeState extends State<Passcode> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(right: 10),
-      child: Container(
-        margin: const EdgeInsets.only(bottom: 8.5),
-        height: 56,
-        width: 54,
-        child: TextFormField(
-          initialValue: widget.passNumber.toString(),
+      margin: const EdgeInsets.all(8.5),
+      decoration: BoxDecoration(
+        color: VotoColors.gray,
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      height: 56,
+      width: 54,
+      child: Center(
+        child: Text(
+          widget.passNumber == ' ' ? '_' : widget.passNumber,
           style: GoogleFonts.inter(
-              color: Colors.black, fontSize: 32, fontWeight: FontWeight.w600),
-          textAlign: TextAlign.center,
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.zero,
-              // hintText: widget.passNumber.toString(),
-              // hintStyle: GoogleFonts.inter(
-              //     color: Colors.black,
-              //     fontSize: 32,
-              //     fontWeight: FontWeight.w600),
-              filled: true,
-              fillColor: Color(0xffF2F4F8),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none)),
-          readOnly: widget.isEditing,
+            color: VotoColors.black,
+            fontSize: 32,
+            fontWeight: FontWeight.w600
+          ),
         ),
       ),
     );
