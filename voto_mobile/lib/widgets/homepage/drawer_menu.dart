b23e1fadea1f5ff587a63887d9fffaa8c75fd008
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-05-17 02:36:13 +0700

    Update image new path

--- voto_mobile/lib/widgets/homepage/drawer_menu.dart
+++ voto_mobile/lib/widgets/homepage/drawer_menu.dart
@@ -1,7 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:voto_mobile/model/persistent_state.dart';
+import 'package:voto_mobile/model/users.dart';
 import 'package:voto_mobile/utils/color.dart';
+import 'package:voto_mobile/utils/random_image.dart';
 import 'package:voto_mobile/widgets/homepage/logout_card.dart';
 import 'package:voto_mobile/widgets/homepage/profile_card.dart';
 
@@ -10,6 +15,12 @@ class DrawerMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String randomImage = RandomImage.get();
+    final user =
+        Provider.of<PersistentState>(context, listen: false).currentUser!;
+    // String? uid =
+    //     Provider.of<PersistentState>(context, listen: false).currentUser?.uid;
+    // DatabaseReference userRef = FirebaseDatabase.instance.ref('users/$uid');
     return Drawer(
       child: ListView(
         // Important: Remove any padding from the ListView.
@@ -23,9 +34,8 @@ class DrawerMenu extends StatelessWidget {
             ),
           ),
           ProfileCard(
-              imagePath: 'assets/user_profile_test.jpg',
-              title:
-                  FirebaseAuth.instance.currentUser?.displayName ?? 'Anonymous',
+              imagePath: user.img ?? randomImage,
+              title: user.displayName ?? 'Anonymous',
               // title: 'Anakin',
               onTap: () {
                 Navigator.pushNamed(context, "/profile_page");
@@ -56,7 +66,7 @@ class DrawerMenu extends StatelessWidget {
           padding: const EdgeInsets.only(left: 20),
           child: Text(
             'Menu',
-             style: GoogleFonts.inter(
+            style: GoogleFonts.inter(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
                 color: VotoColors.indigo),
